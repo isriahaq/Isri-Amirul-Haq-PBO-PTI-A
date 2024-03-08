960b00f185731505a8f83f17834a8d419d0039b4
isriahaq <128814313+isriahaq@users.noreply.github.com>
2024-03-08 12:20:06 +0700

    Create Perpustakaan.java

---
+++ Tugas3/src/Perpustakaan.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Perpustakaan {
+    private List<Buku> daftarBuku;
+    private List<AnggotaPerpustakaan> daftarAnggota;
+
+    public Perpustakaan() {
+        daftarBuku = new ArrayList<>();
+        daftarAnggota = new ArrayList<>();
+    }
+
+    public void tambahBuku(Buku buku) {
+        daftarBuku.add(buku);
+    }
+
+    public void hapusBuku(int idBuku) {
+        for (int i = 0; i < daftarBuku.size(); i++) {
+            if (daftarBuku.get(i).getJudul().equals(idBuku)) {
+                daftarBuku.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void tambahAnggota(AnggotaPerpustakaan anggota) {
+        daftarAnggota.add(anggota);
+    }
+
+    public void hapusAnggota(int idAnggota) {
+        for (int i = 0; i < daftarAnggota.size(); i++) {
+            if (daftarAnggota.get(i).getIdAnggota() == idAnggota) {
+                daftarAnggota.remove(i);
+                break;
+            }
+        }
+    }
+
+    public Buku cariBuku(String judul) {
+        for (Buku buku : daftarBuku) {
+            if (buku.getJudul().equals(judul)) {
+                return buku;
+            }
+        }
+        return null;
+    }
+
+}
