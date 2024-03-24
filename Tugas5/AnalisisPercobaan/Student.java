943728a249025c2535ccb804cb07c49b4da98984
isriahaq <128814313+isriahaq@users.noreply.github.com>
2024-03-24 16:36:41 +0700

    Create Student.java

---
+++ Tugas5/AnalisisPercobaan/Student.java
@@ -0,0 +1,22 @@
+package Tugas5.Analisis;
+
+public class Student {
+    private String name;
+    private int mark;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMark(int m) {
+        mark = m;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
