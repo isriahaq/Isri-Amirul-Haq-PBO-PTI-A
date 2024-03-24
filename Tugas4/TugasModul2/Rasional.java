c8c98b91cda12a807d9a7b2d7046155f6240d09d
isriahaq <128814313+isriahaq@users.noreply.github.com>
2024-03-24 15:55:48 +0700

    Create Rasional.java

---
+++ Tugas4/TugasModul2/Rasional.java
@@ -0,0 +1,121 @@
+package Tugas4.Bab2;
+
+public class Rasional {
+    private int pembilang, penyebut;
+
+    public Rasional() {
+        pembilang = 0;
+        penyebut = 0;
+    }
+
+    public Rasional(int pbl, int pyb) {
+        pembilang = pbl;
+        penyebut = pyb;
+    }
+
+    // mengecek suatu bilangan adalah rasional atau bukan
+    public boolean isRasional() {
+        return (penyebut != 0);
+    }
+
+    // menyederhanakan bilangan rasional
+    public void Sederhana() {
+        int temp, A, B;
+        if (penyebut == 0) {
+            return;
+        }
+        A = (pembilang < penyebut) ? penyebut : pembilang;
+        B = (pembilang < penyebut) ? pembilang : penyebut;
+
+        // while (B != 0) {
+        // temp = A % B;
+        // A = B;
+        // B = temp;
+        // }
+
+        // no 3
+        for (; B != 0;) {
+            temp = A % B;
+            A = B;
+            B = temp;
+        }
+
+        /*inisialisasi;
+         * while(kondisi){
+         * perulangan++;
+         * }
+         */
+        
+        /*for (inisialisasi;kondisi;perulangannya) */
+
+        pembilang /= A;
+        penyebut /= A;
+    }
+
+    public double Cast() {
+        return (penyebut == 0.0) ? 0.0
+                : (double) pembilang /
+                        (double) penyebut;
+    }
+
+    // oprator >
+    public boolean moreThan(Rasional A) {
+        return (pembilang * A.penyebut > penyebut * A.pembilang);
+    }
+
+    // operator Unary- ---> A = -A
+    public void negasi() {
+        pembilang = -pembilang;
+    }
+
+    // operator unary += \
+    public void unaryPlus(Rasional A) {
+        pembilang = pembilang * A.penyebut + penyebut *
+                A.pembilang;
+        penyebut *= A.penyebut;
+    }
+
+    public void cetak() {
+        System.out.println(pembilang + "/" + penyebut);
+    }
+
+    // no 2
+    // operator <
+    public boolean kurangDari(Rasional A) {
+        return (pembilang * A.penyebut < penyebut * A.pembilang);
+    }
+
+    // operator <=
+    public boolean kurangDariAtauSamaDengan(Rasional A) {
+        return (pembilang * A.penyebut <= penyebut * A.pembilang);
+    }
+
+    // operator >=
+    public boolean lebihBesarDariAtauSamaDengan(Rasional A) {
+        return (pembilang * A.penyebut >= penyebut * A.pembilang);
+    }
+
+
+    // no 4
+    // operator -
+    public Rasional subtract(Rasional A) {
+        int pbl = pembilang * A.penyebut - penyebut * A.pembilang;
+        int pyb = penyebut * A.penyebut;
+        return new Rasional(pbl, pyb);
+    }
+
+    // operator *
+    public Rasional multiply(Rasional A) {
+        int pbl = pembilang * A.pembilang;
+        int pyb = penyebut * A.penyebut;
+        return new Rasional(pbl, pyb);
+    }
+
+    // operator /
+    public Rasional divide(Rasional A) {
+        int pbl = pembilang * A.penyebut;
+        int pyb = penyebut * A.pembilang;
+        return new Rasional(pbl, pyb);
+    }
+
+}
