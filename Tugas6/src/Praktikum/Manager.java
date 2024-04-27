f541604638d876d77ed1dcef94af52f59dce984a
isriahaq <128814313+isriahaq@users.noreply.github.com>
2024-04-27 21:54:47 +0700

    Create Manager.java

---
+++ Tugas6/src/Praktikum/Manager.java
@@ -0,0 +1,28 @@
+package src.Praktikum;
+
+import java.time.LocalDate;
+
+public class Manager extends Pekerja {
+    private String departemen;
+
+    public Manager(String nama, boolean jenisKelamin, String nik, boolean menikah, double gaji, LocalDate tahunMasuk, int jumlahAnak, String departemen) {
+        super(nama, jenisKelamin, nik, menikah, gaji, tahunMasuk, jumlahAnak);
+        this.departemen = departemen;
+    }
+
+    public String getDepartemen() {
+        return departemen;
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+
+    public double getPendapatan() {
+        return super.getPendapatan() + (super.getGaji() * 0.1);
+    }
+
+    public String toString() {
+        return super.toString() + ", Departemen: " + departemen;
+    }
+}
