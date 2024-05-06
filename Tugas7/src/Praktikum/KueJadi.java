153042bcd9658a1057e403cb3669500e0dac2bb7
isriahaq <128814313+isriahaq@users.noreply.github.com>
2024-05-06 08:36:03 +0700

    Create KueJadi.java

---
+++ Tugas7/src/Praktikum/KueJadi.java
@@ -0,0 +1,20 @@
+package Tugas7.src.Praktikum;
+
+// Subclass KueJadi
+class KueJadi extends Kue {
+    private double jumlah;
+
+    public KueJadi(String nama, double harga, double jumlah) {
+        super(nama, harga);
+        this.jumlah = jumlah;
+    }
+
+    @Override
+    public double hitungHarga() {
+        return getHarga() * jumlah * 2;
+    }
+
+    public double getJumlah() {
+        return jumlah;
+    }
+}
